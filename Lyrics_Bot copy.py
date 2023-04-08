bf03b63e6cb76e6cd320c6c5bb94e5e4a048652d
Marissa <93284571+mklein805@users.noreply.github.com>
2023-04-08 16:58:43 -0400

    Added comments

--- Lyrics_Bot copy.py
+++ Lyrics_Bot copy.py
@@ -4,32 +4,39 @@
 import tweepy
 import random
 
+#Twitter Authentication
 auth = tweepy.OAuth1UserHandler(
    "API/Consumer Key", "API/Consumer Secret",
    "Access Token", "Access Token Secret"
 )
 api = tweepy.API(auth)
 
+#Read in bot values
 f = open(r'Lyrics.txt','r') #Replace with file of choice
 
 data = f.readlines()
 
 f.close()
 
+#Initialize Random Parameters
 random.seed()
 x = random.randint(0,len(data)-2)
 
+#Check that the last tweet was not the same value
 if x == data[len(data)-1]:
     x = random.randint(0,len(data)-2)
-   
+
+#Call line
 line = data[x]
 
+#Appropriately format line if it has breaks
 line = line.replace(r'\n','\n')
 print(line)
 
-
+#Push the tweet
 api.update_status(status=line)
 
+#Update reference value
 data[len(data)-1] = x
 
 fp = open(r'Lyrics.txt', 'w') #Replace with file of choice
@@ -39,6 +46,7 @@ for item in data:
         
 fp.close()
 
+#Clean up empty space in file
 result = ""
 with open(r"Lyrics.txt", "r+") as file: #Replace with file of choice
     for line in file:
